db1ed87189d8ac795d68b43e21cc7fcd94a8f599
Suchi-QA-sys <suchintan.das@delhivery.com>
2025-03-04 18:15:51 +0530

    Added the tags otherwise its tough to exclude one script and another

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,22 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, tag
 from runner import Runner
+from utils.config_loader import CONFIG
 
 class LoadTest(HttpUser):
     wait_time = between(1, 3)  # Wait time between requests
+    ENABLED_TAGS = CONFIG["enabled_tags"]
 
     def on_start(self):
         self.runner = Runner(self)  # Initialize Runner
         self.runner.setup_auth()  # Fetch auth token first
 
     @task
+    @tag("driver")
     def create_driver(self):
-        self.runner.run_create_driver()
+        if "driver" in ENABLED_TAGS:
+            self.runner.run_create_driver()
 
     @task
+    @tag("attendance")
     def mark_attendance(self):
         self.runner.run_attendance()
